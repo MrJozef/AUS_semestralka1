1d9206dce7cd8f73e15dae11612ce44807659c23
diirector <jozef.kubikml@gmail.com>
2019-03-10 00:11:28 +0100

    Pridane zaciatocne drobnosti

--- semestralka1/const.h
+++ semestralka1/const.h
@@ -1,3 +1,14 @@
 #pragma once
+#include <string>
 #include "../structures/heap_monitor.h"
 
+const int ASCII_CISLO_CISLICA = 48;			//rozdiel medzi cislicou v ascii (char) a cislom, ktore predstavuje (int)
+
+const int POCET_POLOZIEK_MENU = 1;
+
+
+
+enum Region
+{
+	MA, BA, TT, TN, NR, KN, CA, ZA, MT, PD, LV, NO, LM, BB, ZV, KA, LC, PP, RA, SL, SN, PO, KE, HE, MI
+};
--- semestralka1/main.cpp
+++ semestralka1/main.cpp
@@ -4,13 +4,54 @@
 
 using namespace std;
 
+int nacitajInt(int poCislo);
+void Zadavam();
+
 int main()
 {
 	initHeapMonitor();
 
-	cout << "1. semestralna praca - Jozef Kubik\n\n";
-	cout << "Zvolte cislo:\n 0) Koniec" << endl;
+	bool pokracovat = true;
+	while (pokracovat)
+	{
+		cout << "1. semestralna praca - Jozef Kubik\n\n";
+		cout << "Menu:\n  0) Koniec\n\n" << endl;
 
+		switch(nacitajInt(POCET_POLOZIEK_MENU))
+		{
+		case 1:
+			//todo prva moznost
+			break;
+		case 0:
+		default:
+			cout << "\n\n\nKoniec programu - stlacte <Enter> pre ukoncenie" << endl;
+			pokracovat = false;
+		}
+
+	}
+	
 	cin.get();
 	return 0;
 }
+
+
+int nacitajInt(int poCislo)
+{
+	char cislo;
+	cout << "Zadajte cislo od 0 po " + to_string(poCislo) + ":" << endl;
+	Zadavam();
+	cin >> cislo;
+	cin.ignore(1000, '\n');
+
+	while (!((cislo - ASCII_CISLO_CISLICA) <= poCislo && (cislo - ASCII_CISLO_CISLICA) >= 0))
+	{
+		cout << "Zly vstup! Zadajte cislo od 0 po " + to_string(poCislo) + ":" << endl;
+		Zadavam();
+		cin.get(cislo);
+		cin.ignore(1000, '\n');
+	}
+
+	return cislo - ASCII_CISLO_CISLICA;
+}
+
+void Zadavam() { cout << ">> "; }
